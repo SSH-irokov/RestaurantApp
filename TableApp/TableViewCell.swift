87f2f780493d7be41563caee72b9cc98474b622b
gfdtk2016 <pavel2014gfdtk@mail.ru>
2019-11-24 21:54:47 +0300

    custom cell

---
+++ TableApp/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  TableApp
+//
+//  Created by Мария on 24/11/2019.
+//  Copyright © 2019 Павел. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageRestrant: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
